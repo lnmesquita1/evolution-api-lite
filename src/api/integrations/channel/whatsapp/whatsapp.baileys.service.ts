d39c162dcbce204e7718f1bd8c12de9ea94c522d
Lucas Mesquita <lnmesquita1@gmail.com>
2025-01-23 20:09:16 -0300

    Corrige status

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -191,7 +191,7 @@ export class BaileysStartupService extends ChannelStartupService {
   public async getProfileStatus() {
     const status = await this.client.fetchStatus(this.instance.wuid);
 
-    return status?.status;
+    return status[0]?.status;
   }
 
   public get profilePictureUrl() {
@@ -1288,7 +1288,7 @@ export class BaileysStartupService extends ChannelStartupService {
     try {
       return {
         wuid: jid,
-        status: (await this.client.fetchStatus(jid))?.status,
+        status: (await this.client.fetchStatus(jid))[0]?.status,
       };
     } catch (error) {
       return {
